d76f49f56ad086a4ed22d7670d01603100a579eb
vanyog <info@vanyog.com>
2016-07-18 11:48:59 +0300

    Correction to Windows exe

---
+++ Qt-5.7-N.bat
@@ -0,0 +1,6 @@
+echo off
+cls
+set QTDIR=N:\Qt57\5.7\mingw53_32
+set QMAKESPEC=N:\Qt57\5.7\mingw53_32\mkspecs\win32-g++
+set path=N:\Qt57\Tools\mingw530_32\bin;N:\Qt57\5.7\mingw53_32\bin;C:\Program Files (x86)\Git\bin;C:\windows\system32
+C:\Windows\System32\cmd /d
